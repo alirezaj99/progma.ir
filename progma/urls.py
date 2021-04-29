4c9427cfb1296ba623258f408ef9e05d2d06b1c2
alireza <javabvar.a@gmail.com>
2021-04-30 03:17:29 +0430

    start blog app and set its urls

--- progma/urls.py
+++ progma/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 from progma import settings
 
 urlpatterns = [
+    path('', include("blog.urls", namespace="blog")),
     path('admin/', admin.site.urls),
 ]
 
