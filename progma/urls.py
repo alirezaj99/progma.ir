8dade3de311f79422183838f65ad942149ec45ad
alireza <javabvar.a@gmail.com>
2021-05-11 19:16:46 +0430

    add urls file to about_us app

--- progma/urls.py
+++ progma/urls.py
@@ -3,15 +3,14 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from account.views import logout_view
-from about_us.views import about_us
-from decouple import config
+
 
 urlpatterns = [
     path('', include("article.urls", namespace="article")),
     path('', include('social_django.urls', namespace='social')),
     path('', include('subscribers.urls', namespace='subscribers')),
     path('', include('contact.urls', namespace='contact')),
-    path('about-us/', about_us, name='about_us'),
+    path('', include('about_us.urls', namespace='about_us')),
     path('logout/', logout_view, name='logout'),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('admin/', admin.site.urls),
