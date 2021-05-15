3b54bdf76c1d436e467f2f34efa185e0f9be4d05
alireza <javabvar.a@gmail.com>
2021-05-16 01:11:18 +0430

    add reCAPTCHA to subscribe form and fix afew bug

--- subscribers/forms.py
+++ subscribers/forms.py
@@ -1,8 +1,10 @@
 from django import forms
 from .models import Subscribe
+from captcha.fields import ReCaptchaField
 
 
 class SubscribeForm(forms.ModelForm):
+    captcha = ReCaptchaField()
 
     def __init__(self, *args, **kwargs):
         super(SubscribeForm, self).__init__(*args, **kwargs)
